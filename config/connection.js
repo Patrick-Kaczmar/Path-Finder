940d454ffce2df086b85dd5b4f2839eb7678eca1
Zhylow <pk1blue@yahoo.com>
2021-03-25 18:22:28 -0400

    anthony fixed my stuff

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,14 @@
-const Sequelize = require("sequelize");
+let Sequelize = require("sequelize");
 
 let sequelize = new Sequelize("path_finder_db", "root", "Andromeda123", {
     host: "localhost",
     port: 3306,
     dialect: "mysql",
-})
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      }
+});
 
 module.exports = sequelize;
--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,3 @@ CREATE DATABASE path_finder_db;
 
 USE path_finder_db;
 
-CREATE TABLE User (
-	id INTEGER AUTO_INCREMENT NOT NULL,
-    
-    email VARCHAR(50) NOT NULL,
-    
-    password VARCHAR(50) NOT NULL,
-    
-    PRIMARY KEY(id)
-);
-
-SELECT * FROM User;
