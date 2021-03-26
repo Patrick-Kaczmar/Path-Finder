66d634c8875624b2d757f5c718ca552c859a77a9
Zhylow <pk1blue@yahoo.com>
2021-03-26 19:42:33 -0400

    added icons

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
   });
+
+  getLocation = function (result) {
+    window.navigator.geolocation.getCurrentPosition(currentPosition => {
+      let latitude = currentPosition.coords.latitude;
+      let longitude = currentPosition.coords.longitude;
+      result = [latitude, longitude]
+      console.log(result)
+    });
+  }
+
+  getLocation();
+  
 });
